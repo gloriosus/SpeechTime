e71fa9c855f92ef69cfda0ab4e6d4819fab12d35
Vladislav Bondarenko <vladislav@bondarenko.page>
2022-03-10 22:29:27 +1000

    test: add MSTest project; add tests for the SettingsHelper class

---
+++ tests/SpeechTime.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SpeechTime.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SpeechTime.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1961989a-ee49-4376-9474-494d39078cf9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ tests/SpeechTime.Tests/SettingsHelperTest.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using SpeechTime.Helpers;
+using System.Windows.Media;
+
+namespace SpeechTime.Tests
+{
+    [TestClass]
+    public class SettingsHelperTest
+    {
+        [TestMethod]
+        public void GetAbsolutePath_Null_ReturnsAppDirectory()
+        {
+            var expected = AppDomain.CurrentDomain.BaseDirectory;
+
+            var actual = SettingsHelper.GetAbsolutePath(null);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetAbsolutePath_Empty_ReturnsAppDirectory()
+        {
+            var expected = AppDomain.CurrentDomain.BaseDirectory;
+
+            var actual = SettingsHelper.GetAbsolutePath(String.Empty);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetAbsolutePath_WithSlash_ReturnsPathWithoutSlashDuplicates()
+        {
+            var expected = AppDomain.CurrentDomain.BaseDirectory + @"Resources\bleeper.wav";
+
+            var actual = SettingsHelper.GetAbsolutePath(@"\Resources\bleeper.wav");
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetValueOrDefault_SomeText_Returns1()
+        {
+            var expected = 1;
+
+            var actual = SettingsHelper.GetValueOrDefault("SomeText", 1);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetValueOrDefault_5_Returns5()
+        {
+            var expected = 5;
+
+            var actual = SettingsHelper.GetValueOrDefault("5", 1);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetValueOrDefault_SomeText_ReturnsTimeSpanZero()
+        {
+            var expected = TimeSpan.Zero;
+
+            var actual = SettingsHelper.GetValueOrDefault("SomeText", TimeSpan.Zero);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetValueOrDefault_TimeSpan000530_ReturnsTimeSpan000530()
+        {
+            var expected = new TimeSpan(0, 5, 30);
+
+            var actual = SettingsHelper.GetValueOrDefault("00:05:30", TimeSpan.Zero);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetValueOrDefault_SomeText_ReturnsColorAqua()
+        {
+            var expected = Colors.Aqua;
+
+            var actual = SettingsHelper.GetValueOrDefault("SomeText", Colors.Aqua);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetValueOrDefault_00ffff_ReturnsColorAqua()
+        {
+            var expected = Colors.Aqua;
+
+            var actual = SettingsHelper.GetValueOrDefault("#00ffff", Colors.Red);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
