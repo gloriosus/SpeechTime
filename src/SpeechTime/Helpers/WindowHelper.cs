a450a982c29edad8c8c924120aa85640a81ac601
Vladislav Bondarenko <vladislav@bondarenko.page>
2022-03-10 20:57:06 +1000

    fix: use the help method PlaceWindowOnScreen for PanelWindow and TimerWindow

--- src/SpeechTime/Helpers/WindowHelper.cs
+++ src/SpeechTime/Helpers/WindowHelper.cs
@@ -7,16 +7,26 @@ using System.Windows;
 
 namespace SpeechTime.Helpers
 {
-    internal class WindowHelper
+    public static class WindowHelper
     {
-        internal static void PlaceWindowOnScreen(Window window)
+        public static void PlaceWindowOnScreen(Window window)
         {
             var screens = System.Windows.Forms.Screen.AllScreens;
 
-            // TODO: possible error at 'MainWindowLocation'
-            int screenNumber = AppSettings.PanelWindowScreen - 1;
+            string windowName = window.GetType().Name;
 
-            if (screens[screenNumber] != null)
+            int screenNumber = 0;
+
+            if (windowName.Equals("PanelWindow"))
+            {
+                screenNumber = AppSettings.PanelWindowScreen - 1;
+            }
+            else if (windowName.Equals("TimerWindow"))
+            {
+                screenNumber = AppSettings.TimerWindowScreen - 1;
+            }
+
+            if ((screenNumber >= 0 & screenNumber < screens.Length) && screens[screenNumber] != null)
             {
                 window.Left = screens[screenNumber].Bounds.Left;
                 window.Top = screens[screenNumber].Bounds.Top;
